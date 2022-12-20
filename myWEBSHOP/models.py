c5a31ec756df73fa2c979396a178468ac6552875
Tim Dehler <tim_dehler@goodyear.com>
2022-12-20 10:59:25 +0100

    Tim Schule commit

--- myWEBSHOP/models.py
+++ myWEBSHOP/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Users(models.Model):
     userFirstName = models.CharField(max_length=200)
     userLastName = models.CharField(max_length=200)
     userPasswort = models.CharField(max_length=200)
+
+class Producers(models.Model):
+    producerName = models.CharField(max_length=200)
+
+class Bikes(models.Model):
+    bikeType = models.CharField(max_length=200)
+    bikeDescription = models.CharField(max_length=500)
+    #bikeProducerID = models.ForeignKey(Producers, on_delete=models.CASCADE)
+
+class Rentalprocesses(models.Model):
+    rentalprocessStartDate = models.DateField(default=date.today)
+    rentalprocessEndDate = models.DateField()
+    rentalprocessUserID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    #rentalprocessBikeID = models.ForeignKey(Bikes, on_delete=models.CASCADE)
+
